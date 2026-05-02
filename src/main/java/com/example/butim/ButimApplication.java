0a2c1aa2c9b562cefb0d2d3395fe34c69e30fcb0
seomunji <162777421+SEOMUNJI@users.noreply.github.com>
2026-05-02 18:33:03 +0900

    Initial commit

---
+++ src/main/java/com/example/butim/ButimApplication.java
@@ -0,0 +1,13 @@
+package com.example.butim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ButimApplication.class, args);
+    }
+
+}
