fd7f88cfcf9ad0fa2e6fb21b302e82907822d062
seomunji <162777421+SEOMUNJI@users.noreply.github.com>
2026-05-05 17:37:25 +0900

    feat: RegionRepository 생성

---
+++ src/main/java/com/example/butim/domain/region/repository/RegionRepository.java
@@ -0,0 +1,17 @@
+package com.example.butim.domain.region.repository;
+
+import com.example.butim.domain.region.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RegionRepository extends JpaRepository<Region, Long> {
+
+    List<Region> findBySigunguCodeIsNullOrderBySidoNameAsc();
+
+    List<Region> findBySidoCodeAndSigunguCodeIsNotNullOrderBySigunguNameAsc(String sidoCode);
+
+    boolean existsBySidoCodeAndSigunguCode(String sidoCode, String sigunguCode);
+
+    boolean existsBySidoCodeAndSigunguCodeIsNull(String sidoCode);
+}
