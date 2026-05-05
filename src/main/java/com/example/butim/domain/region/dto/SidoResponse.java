22a50b59b629b478697f3d3c5a853d0cdbb3a24c
seomunji <162777421+SEOMUNJI@users.noreply.github.com>
2026-05-05 17:38:42 +0900

    feat: 시도 응답(SidoResponse) DTO 생성

---
+++ src/main/java/com/example/butim/domain/region/dto/SidoResponse.java
@@ -0,0 +1,12 @@
+package com.example.butim.domain.region.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SidoResponse {
+
+    private String sidoCode;
+    private String sidoName;
+}
