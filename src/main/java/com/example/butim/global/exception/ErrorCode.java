6b1f182acfd5c63419ac80398400766dd0f36d9b
seomunji <162777421+SEOMUNJI@users.noreply.github.com>
2026-05-06 19:14:37 +0900

    feat: 업종 및 직종 예외 코드 추가

--- src/main/java/com/example/butim/global/exception/ErrorCode.java
+++ src/main/java/com/example/butim/global/exception/ErrorCode.java
@@ -5,9 +5,13 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-
+    //region
     INVALID_SIDO_CODE(HttpStatus.BAD_REQUEST, "시도 코드는 필수입니다."),
     REGION_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 시도 코드입니다."),
+
+    //industry
+    INDUSTRY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 업종을 찾을 수 없습니다."),
+    JOB_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 업종의 직종 목록을 찾을 수 없습니다."),
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
 
