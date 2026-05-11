e5bbfb512706f3331c5f8c72f69b126cbb2073f7
seomunji <162777421+SEOMUNJI@users.noreply.github.com>
2026-05-11 12:49:30 +0900

    feat: 다양한 소득 범위를 나타내는 IncomeLevel enum 추가

---
+++ src/main/java/com/example/butim/domain/financial/enums/IncomeLevel.java
@@ -0,0 +1,14 @@
+package com.example.butim.domain.financial.enums;
+
+public enum IncomeLevel {
+
+    BELOW_32,          // 32% 이하
+    OVER_32_TO_40,     // 32% 초과 ~ 40% 이하
+    OVER_40_TO_48,     // 40% 초과 ~ 48% 이하
+    OVER_48_TO_50,     // 48% 초과 ~ 50% 이하
+    OVER_50_TO_100,    // 50% 초과 ~ 100% 이하
+    OVER_100_TO_120,   // 100% 초과 ~ 120% 이하
+    OVER_120_TO_150,   // 120% 초과 ~ 150% 이하
+    OVER_150,          // 150% 초과
+    UNKNOWN            // 잘 모르겠어요
+}
