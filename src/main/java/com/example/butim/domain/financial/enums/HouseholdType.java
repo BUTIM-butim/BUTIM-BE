c88308aa7e8060e86b822e3e8a3decc15e738926
seomunji <162777421+SEOMUNJI@users.noreply.github.com>
2026-05-11 12:53:35 +0900

    HouseholdType enum 추가 - 기초생활수급자, 차상위계층, 해당 없음

---
+++ src/main/java/com/example/butim/domain/financial/enums/HouseholdType.java
@@ -0,0 +1,8 @@
+package com.example.butim.domain.financial.enums;
+
+public enum HouseholdType {
+
+    BASIC_LIVELIHOOD_RECIPIENT, // 기초생활수급자
+    NEAR_POVERTY,               // 차상위계층
+    NONE                        // 해당 없음
+}
